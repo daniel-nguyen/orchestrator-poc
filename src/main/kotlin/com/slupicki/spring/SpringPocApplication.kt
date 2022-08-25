6425a94fa88b2898e302ec7c995c19f92b82c98f
Daniel Nguyen <daniel.nguyen-ext@seba.swiss>
2022-08-25 17:18:01 +0200

    creating spring-state version - cleaning up

--- src/main/kotlin/com/slupicki/spring/SpringPocApplication.kt
+++ src/main/kotlin/com/slupicki/spring/SpringPocApplication.kt
@@ -1,6 +1,5 @@
 package com.slupicki.spring
 
-import com.slupicki.spring.service.OnboardingService
 import mu.KotlinLogging
 import org.flywaydb.core.Flyway
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,34 +9,16 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.core.env.Environment
 import org.springframework.core.env.get
-import org.springframework.jms.core.JmsTemplate
 
 @SpringBootApplication
 class SpringPocApplication(
     @Autowired private val environment: Environment,
-    @Autowired private val onboardingService: OnboardingService,
-    @Autowired private val jmsTemplate: JmsTemplate,
-//    @Autowired private val flyway: Flyway,
 ): CommandLineRunner {
     companion object {
         private val log = KotlinLogging.logger {}
     }
 
     override fun run(vararg args: String?) {
-//        flyway.clean()
-//        flyway.migrate()
-
-        //stateMachineEngine.createMachine("ONBOARDING", "client1", mapOf("a" to "value_a"))
-
-//        jmsTemplate.send("test") { it.createTextMessage("Hello world")}
-//        val stateMachine = stateMachineFactory.getStateMachine("1234")
-//        stateMachine.sendEventCollect(Mono.just(MessageBuilder.createMessage()))
-
-
-    }
-    private fun logOnboardingDot() {
-//        val s = graphService.createDot("ONBOARDING", "Atlas")
-//        log.info { s }
     }
 
     @Bean(initMethod = "migrate")
